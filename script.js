19d6e8956413ec973378415aa35e0640816853c5
MadhavBorkar <157888465+MadhavBorkar@users.noreply.github.com>
2024-05-18 00:00:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function removeColor() {
+    const selectElement = document.getElementById('colorSelect');
+    const selectedIndex = selectElement.selectedIndex;
+
+    if (selectedIndex !== -1) {
+        selectElement.remove(selectedIndex);
+    }
+}
